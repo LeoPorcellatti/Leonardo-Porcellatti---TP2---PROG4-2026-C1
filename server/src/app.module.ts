44fc07a47b2254184da890535c970934df58ae37
LeoPorcellatti <leoporcellatti@live.com.ar>
2026-05-24 19:09:25 -0300

    Conexión mongo creada

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FutbolistasModule } from './futbolistas/futbolistas.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [FutbolistasModule],
+  imports: [
+    ConfigModule.forRoot(),
+    FutbolistasModule,
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
