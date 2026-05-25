0a50a1f76a2c3f33668e611e065207e22eddafeb
LeoPorcellatti <leoporcellatti@live.com.ar>
2026-05-25 19:47:34 -0300

    Modulos creados

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,12 +4,18 @@ import { AppService } from './app.service';
 import { FutbolistasModule } from './futbolistas/futbolistas.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PublicacionesModule } from './publicaciones/publicaciones.module';
+import { AutenticacionModule } from './autenticacion/autenticacion.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     FutbolistasModule,
     MongooseModule.forRoot(process.env.MONGO_URI!),
+    PublicacionesModule,
+    AutenticacionModule,
+    UsuariosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
