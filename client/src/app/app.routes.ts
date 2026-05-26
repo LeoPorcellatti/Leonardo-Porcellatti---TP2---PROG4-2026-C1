4ee6bf69d2852da9432f01a1a5012ac11be56b12
LeoPorcellatti <leoporcellatti@live.com.ar>
2026-05-26 18:17:36 -0300

    ruteo primer sprint

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./pantallas/login/login').then((a) => a.Login),
+  },
+  {
+    path: 'mi-perfil',
+    loadComponent: () => import('./pantallas/mi-perfil/mi-perfil').then((a) => a.MiPerfil),
+  },
+
+  {
+    path: 'publicaciones',
+    loadComponent: () =>
+      import('./pantallas/publicaciones/publicaciones').then((a) => a.Publicaciones),
+  },
+  {
+    path: 'registro',
+    loadComponent: () => import('./pantallas/registro/registro').then((a) => a.Registro),
+  },
+];
