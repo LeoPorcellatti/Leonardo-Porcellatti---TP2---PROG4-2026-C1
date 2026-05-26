b7888745677d2ceb2019d291995cbc3a0419c3e3
LeoPorcellatti <leoporcellatti@live.com.ar>
2026-05-26 18:27:27 -0300

    arreglo rutas

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,25 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment.development';
-import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, JsonPipe],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  httpClient = inject(HttpClient);
+  // httpClient = inject(HttpClient);
 
-  futbolistas = signal<any[]>([]);
+  // futbolistas = signal<any[]>([]);
 
   ngOnInit(): void {
-    const peticion = this.httpClient.get(environment.apiUrl + '/futbolistas');
-
-    peticion.subscribe((val) => {
-      this.futbolistas.set(val as any[]);
-    });
+    // const peticion = this.httpClient.get(environment.apiUrl + '/futbolistas');
+    // peticion.subscribe((val) => {
+    //   this.futbolistas.set(val as any[]);
+    // });
   }
 }
