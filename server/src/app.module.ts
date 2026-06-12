a8cc2c078a2f02bf79aebc79ade25664950d5690
LeoPorcellatti <leoporcellatti@live.com.ar>
2026-06-11 23:31:04 -0300

    Imagen de perfil por defecto en antiguos

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FutbolistasModule } from './futbolistas/futbolistas.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PublicacionesModule } from './publicaciones/publicaciones.module';
@@ -11,7 +10,6 @@ import { UsuariosModule } from './usuarios/usuarios.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    FutbolistasModule,
     MongooseModule.forRoot(process.env.MONGO_URI!),
     PublicacionesModule,
     AutenticacionModule,
